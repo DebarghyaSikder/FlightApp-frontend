1033e52f4078b823f49660fbdeda1efbe4515dc3
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-16 00:54:07 +0530

    Added signup component

---
+++ src/app/components/signup/signup.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { userDetails } from '../../models/userDetails';
+import { UserRole } from '../../enums/user-role.enum';
+import { AuthService } from '../../services/Authentication/auth-service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { usernameValidator } from '../../validatorFunctions/usernameValidator';
+import { passwordValidator } from '../../validatorFunctions/passwordValidator';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-signup',
+  imports: [ReactiveFormsModule,AsyncPipe,RouterModule],
+  templateUrl: './signup.html',
+  standalone:true,
+  styleUrl: './signup.css',
+})
+export class Signup {
+  error$!: Observable<string | null>;
+  constructor(private readonly auth:AuthService,private readonly router:Router){}
+  ngOnInit(): void {
+    this.auth.clearError();
+    this.error$ = this.auth.error$;
+  }
+  isroleSelected:boolean=false;
+  selectedRole:string="";
+  UserRole = UserRole;
+  userDetails:userDetails={
+    username:"",
+    email:"",
+    password:"",
+    roles :[UserRole.ROLE_USER],
+  }
+form=new FormGroup(
+{
+  email:new FormControl('',[Validators.required,Validators.email]),
+  username:new FormControl('',[Validators.required,usernameValidator]),
+  password: new FormControl('',[Validators.required,passwordValidator])
+
+
+}
+);
+
+  selectRole(roleSelected: UserRole): void {
+    this.isroleSelected=true;
+  this.userDetails.roles = [roleSelected];
+  if(this.userDetails.roles.includes(UserRole.ROLE_ADMIN)){
+    this.selectedRole="Admin";
+  }else{
+    this.selectedRole="User";
+  }
+}
+submit() {
+  this.userDetails.username = this.form.value.username!;
+  this.userDetails.email = this.form.value.email!;
+  this.userDetails.password = this.form.value.password!;
+
+  this.auth.signup(this.userDetails).subscribe({
+    next: () => {
+      console.log('Signup successful');
+       this.auth.clearError();
+      this.router.navigate(['/signin']);
+    }
+  });
+}
+
+}
