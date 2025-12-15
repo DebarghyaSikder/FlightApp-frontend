6fd3d2081cbd62f74b17e669c76dde6d8db9ec48
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-15 23:29:35 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SearchFlightsComponent } from './flights/search/search.component';
+import { FlightResultsComponent } from './flights/results/results.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: 'search-flights', component: SearchFlightsComponent },
+  { path: 'flight-results', component: FlightResultsComponent }
+];
