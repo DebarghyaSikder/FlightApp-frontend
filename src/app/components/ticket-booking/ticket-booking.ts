7b7a17cafd3c1e7090d5b7639b7e8b55cd7d74f9
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-23 00:58:19 +0530

    Changes in ticket booking

--- src/app/components/ticket-booking/ticket-booking.ts
+++ src/app/components/ticket-booking/ticket-booking.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TicketList } from '../ticket-list/ticket-list';
 import {
   Observable,
   Subject,
@@ -11,13 +12,14 @@ import {
 
 import { TicketService } from '../../services/TicketService/ticket-service';
 import { Ticket } from '../../models/Ticket';
-import { AuthService } from '../../services/Authentication/auth.service';
-import { UserResponse } from '../../models/UserResponse';
-
+import { AuthService } from '../../services/Authentication/auth-service';
+import { PassengerService } from '../../services/PassengerService/passenger-service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { searchingStateService } from '../../services/SavingStates/searchingStateService';
 @Component({
   selector: 'app-ticket-booking',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, TicketList],
   templateUrl: './ticket-booking.html',
   styleUrl: './ticket-booking.css',
 })
@@ -26,53 +28,76 @@ export class TicketBooking implements OnInit {
   tickets$!: Observable<Ticket[]>;
   private refresh$ = new Subject<void>();
 
+  ticketBookedMessage$ = new Subject<string>();
+
   flightId!: number;
-  passengerId!: number;
-  seatNo!: string;
+  numberOfSeats!: number;
 
   constructor(
     private readonly ticketService: TicketService,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly passengerService: PassengerService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+    private readonly searchingState: searchingStateService
   ) {}
 
   ngOnInit(): void {
-  this.flightId = history.state?.flightId;
-  console.log('Flight ID:', this.flightId);
+    this.flightId = Number(this.route.snapshot.queryParamMap.get('flightId'));
 
-  this.tickets$ = this.refresh$.pipe(
-    startWith(void 0),
-    switchMap(() =>
-      this.authService.currentUser.pipe(
-        take(1),
-        switchMap((user): Observable<Ticket[]> => {
-          if (!user) {
-            throw new Error('User not logged in');
-          }
-          const typedUser = user as UserResponse;
+    if (!this.flightId) {
+      this.router.navigate(['/']);
+      return;
+    }
 
-          this.passengerId = typedUser.id;
-          return this.ticketService.getTicketsByEmail(typedUser.email);
-        })
+    this.tickets$ = this.refresh$.pipe(
+      startWith(void 0),
+      switchMap(() =>
+        this.authService.currentUser.pipe(
+          take(1),
+          switchMap(user => {
+            if (!user) {
+              throw new Error('User not logged in');
+            }
+            return this.ticketService.getTicketsByEmail(user.email);
+          })
+        )
       )
-    )
-  );
+    );
+  }
+  onTicketCancelled() {
+  this.refresh$.next();
 }
 
-
   bookTicket() {
-    this.ticketService
-      .bookTicketByPassengerIdandFlightId(
-        this.flightId,
-        this.passengerId,
-        this.seatNo
+    if (!this.numberOfSeats) return;
+
+    this.authService.currentUser.pipe(
+      take(1),
+      switchMap(user => {
+        if (!user) {
+          throw new Error('User not logged in');
+        }
+        return this.passengerService.getPassengerIdByEmail(user.email);
+      }),
+      switchMap(passengerId =>
+        this.ticketService.bookTicketByPassengerIdandFlightId(
+          this.flightId,
+          passengerId,
+          this.numberOfSeats
+        )
       )
-      .subscribe({
-        next: () => {
-          console.log('Ticket booked');
-          this.refresh$.next();
-          this.seatNo = '';
-        },
-        error: err => console.error('Booking failed', err),
-      });
+    ).subscribe({
+      next: () => {
+        this.searchingState.clear();
+        this.ticketBookedMessage$.next('Ticket booking successful');
+        this.numberOfSeats = 0;
+        this.refresh$.next();
+      },
+      error: err => {
+        console.error('Booking failed', err);
+        this.ticketBookedMessage$.next('Ticket booking failed');
+      }
+    });
   }
 }
